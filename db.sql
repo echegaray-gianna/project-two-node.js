cf2c153ea1614930652e74f289f1113f30fd3f13
Giannagp <giannagp@users.noreply.github.com>
2020-11-20 21:58:35 -0700

    project two - prove 10

--- db.sql
+++ db.sql
@@ -6,7 +6,8 @@ CREATE TABLE category (
 
 CREATE TABLE streaming (
     streaming_id SERIAL PRIMARY KEY NOT NULL,
-    streaming_name VARCHAR (200)
+    streaming_name VARCHAR (200),
+    streaming_img VARCHAR(200)
 )
 
 
@@ -50,41 +51,81 @@ INSERT INTO category (category_name) VALUES ('Documentaries');
 
 
 
-ALTER TABLE streaming ADD streaming_img VARCHAR(200);
 
-INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Netflix', '/img/netflix.png');
+INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Netflix', '/img/netflix.jpg');
 
 INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Disney Plus', '/img/disney.jpg');
 
-INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Prime Video', '/img/prime.png');
+INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Prime Video', '/img/prime.jpg');
 
 INSERT INTO streaming (streaming_name, streaming_img ) VALUES ('Hulu', '/img/hulu.jpg');
 
 
+-- DONE
+
+UPDATE streaming SET streaming_img = '/img/netflix.jpg' WHERE streaming_id = 1;
+
+UPDATE streaming SET streaming_img = '/img/disney.jpg' WHERE streaming_id = 2;
+
+UPDATE streaming SET streaming_img = '/img/prime.jpg' WHERE streaming_id = 3;
+
+UPDATE streaming SET streaming_img = '/img/hulu.jpg' WHERE streaming_id = 4;
+
+
+-- DONE
+ALTER TABLE movie ALTER COLUMN movie_date SET DATA type VARCHAR (50);
 
 
 INSERT INTO movie(
-    movie_name, movie_date, movie_actors, movie_duration, movie_description, movie_img)
-    VALUES ('The Lion King', '2019-07-19', 'Donald Glover, Beyonce, Seth Rogen, Billy Eichnner, Chiwetel Ejiofor', '01:58:00', 
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/lion.jpg', 'The Lion King', '2019-07-19', 'https://www.youtube.com/watch?v=7TavVZMewpY', 
+     'Donald Glover, Beyonce, Seth Rogen, Billy Eichnner, Chiwetel Ejiofor', '01:58:00', 
     'Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny on the plains of Africa. But not everyone in the kingdom celebrates the new cubs arrival. Scar, Mufasa"s brother -- and former heir to the throne -- has plans of his own. The battle for Pride Rock is soon ravaged with betrayal, tragedy and drama, ultimately resulting in Simba"s exile. Now, with help from a curious pair of newfound friends, Simba must figure out how to grow up and take back what is rightfully his.',
-    '/projecttwo/img/lion.jpg')
+    '1', '2');
 
-
+ 
 INSERT INTO movie(
-    movie_name, movie_date, movie_actors, movie_duration, movie_description, movie_img)
-    VALUES ('Beauty and the Beast', '2017-03-17', 'Emma Watson, Dan Stevens, Luke Evans, Josh Gad, Emma Thompson', '02:19:00', 
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/beauty.jpg', 'Beauty and the Beast', '2017-03-17', 'https://www.youtube.com/watch?v=k-AyhL9AR9Q', 
+    'Emma Watson, Dan Stevens, Luke Evans, Josh Gad, Emma Thompson', '02:19:00', 
     'An arrogant prince is cursed to live as a terrifying beast until he finds true love. Strangely, his chance comes when he captures an unwary clockmaker, whose place is then taken by his bold and beautiful daughter Belle. Helped by the Beast"s similarly enchanted servants, including a clock, a teapot and a candelabra, Belle begins to see the sensitive soul behind the fearsome facade. But as time runs out, it soon becomes obvious that Belle"s cocky suitor Gaston is the real beast of the piece',
-    '/projecttwo/img/beauty.jpg')
+    '1', '2');
 
 INSERT INTO movie(
-    movie_name, movie_date, movie_actors, movie_duration, movie_description, movie_img)
-    VALUES ('Onward', '2020-03-06', 'Tom Holland, Chris Pratt, Octavia Spencer, Julia Louis, Ali Wong', '01:42:00', 
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/onward.jpg', 'Onward', '2020-03-06', 'https://www.youtube.com/watch?v=gn5QmllRCn4',
+    'Tom Holland, Chris Pratt, Octavia Spencer, Julia Louis, Ali Wong', '01:42:00', 
     'Teenage elf brothers Ian and Barley embark on a magical quest to spend one more day with their late father. Like any good adventure, their journey is filled with cryptic maps, impossible obstacles and unimaginable discoveries. But when dear Mom finds out her sons are missing, she teams up with the legendary manticore to bring her beloved boys back home.',
-    '/projecttwo/img/onward.jpg')
+    '1', '2');
 
     INSERT INTO movie(
-    movie_name, movie_date, movie_actors, movie_duration, movie_description, movie_img)
-    VALUES ('Spider-Man: Into the Spider-Verse', '2018-12-14', 'Shameik Moore, Hailee Steinfeld, Jake Jhonson, Nicolas Cage', '01:56:00', 
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/spider.jpg', 'Spider-Man: Into the Spider-Verse', '2018-12-14', 'https://www.youtube.com/watch?v=g4Hbz2jLxvQ',
+    'Shameik Moore, Hailee Steinfeld, Jake Jhonson, Nicolas Cage', '01:56:00', 
     'Bitten by a radioactive spider in the subway, Brooklyn teenager Miles Morales suddenly develops mysterious powers that transform him into the one and only Spider-Man. When he meets Peter Parker, he soon realizes that there are many others who share his special, high-flying talents. Miles must now use his newfound skills to battle the evil Kingpin, a hulking madman who can open portals to other universes and pull different versions of Spider-Man into our world.',
-    '/projecttwo/img/spider.jpg')
+    '2', '1');
+
+    INSERT INTO movie(
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/gemini.jpg', 'Gemini Man', '2019-10-03', 'https://www.youtube.com/watch?v=AbyJignbSj0',
+    'Will Smith, Mary Winstead, Clive Owen, Benedic Wong', '01:57:00', 
+    'Henry Brogan is an elite 51-year-old assassin who"s ready to call it quits after completing his 72nd job. His plans get turned upside down when he becomes the target of a mysterious operative who can seemingly predict his every move. To his horror, Brogan soon learns that the man who"s trying to kill him is a younger, faster, cloned version of himself.',
+    '3', '4');
+
+    INSERT INTO movie(
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/suits.jpg', 'Suits', '2019-10-03', 'https://www.youtube.com/watch?v=SYUQKm2nZNE',
+    'Gabriel Macht, Meghan Duchess of Sussex,  Patrick J. Ademas, Sarah Rafferty, Rick Hoffman', '00:43:00', 
+    'Big-time Manhattan corporate lawyer Harvey Specter and his team, which includes Donna Paulsen, Louis Litt, and Alex Williams, are launched into a play for power when a new partner joins the firm. With his two best associates gone and Jessica back in Chicago, Specter and the team try to adjust to a new normal without them. The team faces down betrayals, fiery relationships, and secrets that eventually come to light. Old and new rivalries surface among members of the team as they learn to deal with their new member.',
+    '5', '3');
+
+    INSERT INTO movie(
+    movie_img, movie_name, movie_date, movie_trailer, movie_actors, movie_duration, movie_description, category_id, streaming_id)
+    VALUES ('/img/social.jpg', 'The Social Dilemma', '2020-01-26', 'https://www.youtube.com/watch?v=uaaC57tcci0',
+    'Skyler Gisondo, Jaron Lanier, Kara Hayward', '01:34:00', 
+    'Tech experts sound the alarm on the dangerous human impact of social networking.',
+    '6', '1');
+
+    --DONE ^^^^^
+
 
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 //Port
 app.set("port", (process.env.PORT || 5000));
@@ -130,6 +134,191 @@ function getstreamingFromDb(callback) {
   //DB
   var sql = "SELECT * FROM streaming";
   pool.query(sql, function (err, result) {
+    if (err) {
+      console.log("An error with db ocurred");
+      console.log(err);
+      callback(err, null);
+    }
+
+    console.log("found Db result:" + JSON.stringify(result.rows));
+    callback(null, result.rows);
+
+  });
+}
+
+
+////////////////////////////////////////
+////// GET Movie by Category //////////
+///////////////////////////////////////
+
+//get movie by categories
+
+app.get('/getmoviebycategory', getmoviebycategory);
+
+function getmoviebycategory(req, res) {
+  console.log("getting movies by categories information ");
+
+
+  const category_id = req.query.category_id;
+
+  getmoviebycategoryFromDb(category_id, function (error, result) {
+    if (error) {
+      response.status(500).json({
+        success: false,
+        data: error
+      });
+
+    } else {
+      //info is correct
+
+      const params = {
+        moviecat: result
+      };
+      
+      res.render("pages/getmoviebycategory", params)
+
+      console.log("back from the getmoviebycategoryFromDB with result:", result);
+
+
+    }
+  })
+}
+
+//get movies from DB
+
+function getmoviebycategoryFromDb(category_id, callback) {
+  //to know where the info is from
+  console.log("get movies by categories from Db");
+
+  //DB
+  var sql = "SELECT * FROM movie WHERE category_id = $1::int ";
+  
+  var params = [category_id];
+
+  pool.query(sql, params, function (err, result) {
+    if (err) {
+      console.log("An error with db ocurred");
+      console.log(err);
+      callback(err, null);
+    }
+
+    console.log("found Db result:" + JSON.stringify(result.rows));
+    callback(null, result.rows);
+
+  });
+}
+
+
+////////////////////////////////////////
+////// GET Movie by Streaming //////////
+///////////////////////////////////////
+
+
+app.get('/getmoviebystreaming', getmoviebystreaming);
+
+function getmoviebystreaming(req, res) {
+  console.log("getting movies by streaming information ");
+
+  const streaming_id = req.query.streaming_id;
+
+
+  getmoviebystreamingFromDb(streaming_id, function (error, result) {
+    if (error) {
+      response.status(500).json({
+        success: false,
+        data: error
+      });
+
+    } else {
+      //info is correct
+
+      const params = {
+        moviestr: result
+      };
+      
+      res.render("pages/getmoviebystreaming", params)
+
+      console.log("back from the getmoviebystreamingFromDB with result:", result);
+
+
+    }
+  })
+}
+
+//get categories from DB
+
+function getmoviebystreamingFromDb(streaming_id, callback) {
+  //to know where the info is from
+  console.log("get movies by streaming from Db");
+
+  //DB
+  var sql = "SELECT * FROM movie WHERE streaming_id = $1::int ";
+  
+  var params = [streaming_id];
+
+  pool.query(sql, params, function (err, result) {
+    if (err) {
+      console.log("An error with db ocurred");
+      console.log(err);
+      callback(err, null);
+    }
+
+    console.log("found Db result:" + JSON.stringify(result.rows));
+    callback(null, result.rows);
+
+  });
+}
+
+
+
+////////////////////////////////////////
+///////////// GET Movie ///////////////
+///////////////////////////////////////
+
+
+app.get('/getmovie', getmovie);
+
+function getmovie(req, res) {
+  console.log("getting movies by movies information ");
+
+  const movie_id = req.query.movie_id;
+
+
+  getmovieFromDb(movie_id, function (error, result) {
+    if (error) {
+      response.status(500).json({
+        success: false,
+        data: error
+      });
+
+    } else {
+      //info is correct
+
+      const params = {
+        movieinfo: result
+      };
+      
+      res.render("pages/getmovie", params)
+
+      console.log("back from the getmovieFromDB with result:", result);
+
+
+    }
+  })
+}
+
+//get movie from DB
+
+function getmovieFromDb(movie_id, callback) {
+  //to know where the info is from
+  console.log("get movie by movies from Db");
+
+  //DB
+  var sql = "SELECT * FROM movie WHERE movie_id = $1::int ";
+  
+  var params = [movie_id];
+
+  pool.query(sql, params, function (err, result) {
     if (err) {
       console.log("An error with db ocurred");
       console.log(err);
